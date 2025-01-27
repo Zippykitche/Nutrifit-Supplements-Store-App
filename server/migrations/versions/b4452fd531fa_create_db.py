81df4164ff590e9e9255375e619b0ab490115a9e
zippykitche <zippyk80@gmail.com>
2025-01-28 02:18:53 +0300

    initial commit

---
+++ server/migrations/versions/b4452fd531fa_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: b4452fd531fa
+Revises: 
+Create Date: 2025-01-28 01:44:54.498579
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b4452fd531fa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
