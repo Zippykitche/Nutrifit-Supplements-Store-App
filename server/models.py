402f0472565995c97cebcb00d36e99c80830a23f
zippykitche <zippyk80@gmail.com>
2025-07-03 23:33:52 +0300

    added server.config in models.py

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from config import db
+from server.config import db
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from datetime import datetime
